828289d0eff2201fb87921d7f3b15a9e67df9f40
Dan Coakes <dancoakes@gmail.com>
2026-05-26 16:02:17 +0000

    Prevent PWA update reload loop

--- apps/web/src/pwa.ts
+++ apps/web/src/pwa.ts
@@ -1,13 +1,6 @@
 export function registerPwa() {
   if (!('serviceWorker' in navigator)) return
 
-  let refreshing = false
-  navigator.serviceWorker.addEventListener('controllerchange', () => {
-    if (refreshing) return
-    refreshing = true
-    window.location.reload()
-  })
-
   navigator.serviceWorker.register('/sw.js').then(registration => {
     registration.update().catch(() => undefined)
     setInterval(() => registration.update(), 10 * 60 * 1000)
